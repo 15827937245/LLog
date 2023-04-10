8899bda2b6573a5bb6836d564a981126cfdd7e9d
15827937245 <1803440112@qq.com>
2023-04-10 17:20:21 +0800

    LLog 修复闪退问题

--- app/src/main/java/com/llthx/llog/MainActivity.java
+++ app/src/main/java/com/llthx/llog/MainActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    public String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LLog.i(TAG,"onCreate()");
     }
 }
